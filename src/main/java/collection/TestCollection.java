55fd375a3bfa62e85563f1c16577a227f22608cf
momoubin <moubin.mo@zuolin.com>
2019-11-05 08:59:18 +0800

    test collection

---
+++ src/main/java/collection/TestCollection.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.ArrayList;
+
+/**
+ * @author moubin.mo
+ * @date: 2019/11/3 21:22
+ */
+
+public class TestCollection {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(11);
+	}
+}
