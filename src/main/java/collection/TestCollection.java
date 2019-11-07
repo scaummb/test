7e33c5862064913d0b132ff8d9aaf2dcb9040304
momoubin <moubin.mo@zuolin.com>
2019-11-07 20:37:31 +0800

    backup:test Multimap

--- src/main/java/collection/TestCollection.java
+++ src/main/java/collection/TestCollection.java
@@ -1,6 +1,9 @@
 package collection;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author moubin.mo
@@ -8,8 +11,17 @@ import java.util.ArrayList;
  */
 
 public class TestCollection {
+	public static final String GATEWAY_KEY = "gateway_login";
+
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(11);
+		String tokenString = getCacheKey(GATEWAY_KEY, "tokenString");
+		System.out.println(tokenString);
+
+
+	}
+	private static String getCacheKey(Object... keys) {
+		return StringUtils.join(Arrays.asList(keys), ":");
 	}
 }
---
+++ src/main/java/collection/TestMultimap.java
@@ -0,0 +1,27 @@
+package collection;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Map;
+
+/**
+ * @author moubin.mo
+ * @date: 2019/11/7 20:16
+ */
+
+public class TestMultimap {
+	public static void main(String[] args) {
+		DeferredResult result = new DeferredResult();
+		DeferredResult result2 = new DeferredResult();
+		Multimap multimap = Multimaps.synchronizedSetMultimap( HashMultimap.create() );
+		multimap.put("mmb", result);
+		multimap.put("mmb", result2);
+		Map map = multimap.asMap();
+		System.out.println(map);
+
+
+	}
+}
