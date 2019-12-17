7df35c77e61c558046b08cd5b3a65aac9d92f737
momoubin <moubin.mo@zuolin.com>
2019-12-17 16:38:33 +0800

    singleton testcase 6种单例模式写法用例

---
+++ src/main/java/design/SinglePattern/SingletonCase4.java
@@ -0,0 +1,40 @@
+package design.SinglePattern;
+
+/**
+ * 双锁检验
+ * 双重非空判断，new对象前加一次锁。
+ * volatile关键字，考虑的是，new关键字在虚拟机中执行时其实分为很多步骤，具体原因可以参考深入理解java虚拟机一书（考虑的是这个new关键字字节码执行时是非原子性的），而volatile关键字可以防止指令重排。
+ * @author moubin.mo
+ * @date: 2019/12/17 16:11
+ */
+
+public class SingletonCase4 {
+
+	/**volatile防止指令重排*/
+	private static volatile SingletonCase4 singleton;
+
+	public SingletonCase4() {
+	}
+
+	/**
+	 * 只是在实例为空时才进行同步创建
+	 * 为什么做了2次判断？
+	 * A线程和B线程同时进入同步方法0
+	 * 然后都在1位置处判断了实例为null
+	 * 然后都进入了同步块2中
+	 * 然后A线程优先进入了同步代码块2中（B线程也进入了），然后创建了实例
+	 * 此时，如果没有3处的判断，那么A线程创建实例同时，B线程也会创建一个实例
+	 * 所以，还需要做2次判断
+	 * */
+	public synchronized static SingletonCase4 getInstance(){
+		if (singleton == null){
+			synchronized (SingletonCase4.class){
+				if (singleton == null){
+					singleton = new SingletonCase4();
+				}
+			}
+		}
+		return singleton;
+	}
+
+}
---
+++ src/main/java/design/SinglePattern/SingletonCase5.java
@@ -0,0 +1,23 @@
+package design.SinglePattern;
+
+/**
+ * 内部类
+ * 优点：由于静态内部类跟外部类是平级的，所以外部类加载的时候不会影响内部类，因此实现了lazy loading, 同时也是利用静态变量的方式，使得INSTANCE只会在SingletonHolder加载的时候初始化一次，从而保证不会有多线程初始化的情况，因此也是线程安全的。
+ * @author moubin.mo
+ * @date: 2019/12/17 16:11
+ */
+
+public class SingletonCase5 {
+	//静态内部类,在被用到时才加载（根据内部类不会在其外部类被加载的同时被加载的事实）
+	private static class SingletonCase5Holder{
+		private static final SingletonCase5 singleton = new SingletonCase5();
+	}
+
+	public SingletonCase5() {
+	}
+
+	public static SingletonCase5 getInstance(){
+		return SingletonCase5Holder.singleton;
+	}
+
+}
