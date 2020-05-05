2b4b7560c0237e4c77be43b425bec83c8f887147
scaummb <201330290918a>
2020-05-05 17:19:43 +0800

    避免在æfix ³¨释里添加Unicode字符

---
+++ src/main/java/unicodetest/TestUnicodeNote.java
@@ -0,0 +1,15 @@
+package unicodetest;
+
+/**
+ * <p>避免在注释里添加Unicode字符</p>
+ * @author momoubin
+ * @date 2020/5/5
+ */
+public class TestUnicodeNote {
+    public static void main(String[] args) {
+        Integer id = 1111;
+        // \u000d id = 2222;
+        System.out.println(id);
+        // \u000d // 阿斯蒂芬
+    }
+}
