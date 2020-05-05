dc35dfa37c9f2caa88b3766f30a4a27a2e030fe5
scaummb <201330290918a>
2020-05-05 15:50:51 +0800

    fix 比较工具类两种排序方法的性能

---
+++ src/main/java/collection/testComparatorAndCollectionUtils/User.java
@@ -0,0 +1,27 @@
+package collection.testComparatorAndCollectionUtils;
+
+/**
+ * @author momoubin
+ * @date 2020/5/5
+ */
+public class User implements Comparable {
+    private Long age;
+
+    public User(Long age) {
+        this.age = age;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        User obj  = (User) o;
+        return Long.compare(age, obj.getAge());
+    }
+}
