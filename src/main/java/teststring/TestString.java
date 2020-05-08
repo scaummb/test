f69ec98069f2d141ee57341ff0427bb925db619d
scaummb <201330290918a>
2020-05-09 03:03:33 +0800

    fix 新版本的解密url

--- src/main/java/teststring/TestString.java
+++ src/main/java/teststring/TestString.java
@@ -16,7 +16,14 @@ public class TestString {
         try {
             String[] data = routerUrl.split("url=");
             String decodeString = URLDecoder.decode(data[1], "UTF-8");
-            PrintUtils.printObjects(decodeString);
+//            PrintUtils.printObjects(decodeString);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String aa = "zl://enterprise/member-apply?url=http%3A%2F%2Fgonggdev2.zuolin.com%2Fuser-identification%2Fbuild%2Findex.html%23%2FenterpriseApply%3FuserId%3D550500%26namepaceId%3D11%26requestId%3D9%23sign_suffix&clientHandlerType=2";
+        try {
+            String decode = URLDecoder.decode(aa, "utf-8");
+            PrintUtils.printObjects(decode);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
