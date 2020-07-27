96315549968fbed3cec060553b63f034639db752
momoubin <moubin.mo@zuolin.com>
2020-07-27 15:41:14 +0800

    fix:map的key值类型不一致，获取不到目标值

---
+++ src/main/java/map/TestKeyType.java
@@ -0,0 +1,34 @@
+package map;
+
+import org.springframework.util.ObjectUtils;
+
+import java.util.HashMap;
+
+/**
+ * @author moubin.mo
+ * @date: 2020/7/27 15:36
+ */
+
+public class TestKeyType {
+	public static void main(String[] args) {
+		HashMap integerhashMap = new HashMap<Integer, String>(){{put(100, "100");}};
+		HashMap longhashMap = new HashMap<Long, String>(){{put(100L, "100");}};
+		if (!ObjectUtils.isEmpty(integerhashMap.get(100))){
+			System.out.println("intmap get int: 100");
+		}
+
+		if (!ObjectUtils.isEmpty(integerhashMap.get(100L))){
+			System.out.println("intmap get long: 100");
+		}
+
+		if (!ObjectUtils.isEmpty(longhashMap.get(100))){
+			System.out.println("longmap get int: 100");
+		}
+
+		if (!ObjectUtils.isEmpty(longhashMap.get(100L))){
+			System.out.println("longmap get long: 100");
+		}
+
+
+	}
+}
