f76eb27b2a3a3420dbdfb9968876bac637583369
momoubin <moubin.mo@zuolin.com>
2020-07-27 15:42:36 +0800

    fix:map的key值类型不一致，获取不到目标值

--- src/main/java/map/TestKeyType.java
+++ src/main/java/map/TestKeyType.java
@@ -11,17 +11,18 @@ import java.util.HashMap;
 
 public class TestKeyType {
 	public static void main(String[] args) {
+		Long key = 100L;
 		HashMap integerhashMap = new HashMap<Integer, String>(){{put(100, "100");}};
 		HashMap longhashMap = new HashMap<Long, String>(){{put(100L, "100");}};
 		if (!ObjectUtils.isEmpty(integerhashMap.get(100))){
 			System.out.println("intmap get int: 100");
 		}
 
-		if (!ObjectUtils.isEmpty(integerhashMap.get(100L))){
+		if (!ObjectUtils.isEmpty(integerhashMap.get(key.intValue()))){
 			System.out.println("intmap get long: 100");
 		}
 
-		if (!ObjectUtils.isEmpty(longhashMap.get(100))){
+		if (!ObjectUtils.isEmpty(longhashMap.get(key.intValue()))){
 			System.out.println("longmap get int: 100");
 		}
 
