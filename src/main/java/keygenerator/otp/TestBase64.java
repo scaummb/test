202d9af0ab18b9697364229fb6fa7504af9b499a
momoubin <moubin.mo@zuolin.com>
2020-10-21 11:33:29 +0800

    fix:base64 testcase

--- src/main/java/keygenerator/otp/TestBase64.java
+++ src/main/java/keygenerator/otp/TestBase64.java
@@ -4,6 +4,8 @@ import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author moubin.mo
@@ -11,10 +13,33 @@ import java.io.IOException;
  */
 
 public class TestBase64 {
+	final static BASE64Encoder encoder = new BASE64Encoder();
+	final static BASE64Decoder decoder = new BASE64Decoder();
 	public static void main(String[] args) throws IOException {
+		Base64Decode();
+	}
 
-		final BASE64Encoder encoder = new BASE64Encoder();
-		final BASE64Decoder decoder = new BASE64Decoder();
+	private static void testDeCodeSmartQrCode() {
+		String encodeCode = "NTEBETAwMDQ1MzE1MDAwMDExNDk5AocBAYBXtXWZJWCYXTqDZWShpYqbCvt4ySidK8a6aTcbYuwbtDcaqXtbdp5H3Q9HAAZS9cn/qxQxGwBPihHaccUNJ/dcGQDXsKvpI0RW/w9AIS3FDbzJw7MSqQM1VZVEuF2CB87+tHTIHJ3l/bTc2U6ztG9USHBJW4PJDFtjjjUmpdGJMlq0LY8=";
+
+
+	}
+
+	private static void testDoorDecode() throws IOException {
+		String qrCodeKey = "AQGAvTXy4K4HlLNdD//GbR4hWNvLQ2h5wZzXk8ZGMS9Et5kzPyl7KwSr69lf35h8Yawcp0KI8L7dMiKvOFtL+EktGlEKkblv6IN8qIhCvI2gYjctooIiyGCcm7BuET93GElPEK2rJsQkMGm4E2q/DTk/mVNQvXrYFYwrdOC7RpN95GA/=";
+		byte[] bytes = decoder.decodeBuffer(qrCodeKey);
+		String value = new String(bytes);
+		System.out.println(value);
+	}
+
+	private final static void testBase64EnCode(){
+		String string = "51";
+		String encode = encoder.encode(string.getBytes());
+		System.out.println(encode);
+	}
+
+	private final static void test() throws IOException {
+
 		final String text = "字串文字";
 		final byte[] textByte = text.getBytes("UTF-8");
 		//编码
@@ -23,6 +48,43 @@ public class TestBase64 {
 		//解码
 		String decodeText = new String(decoder.decodeBuffer(encodedText), "UTF-8");
 		System.out.println(decodeText);
+	}
 
+	private final static String Base64Decode(){
+		String string = "NTEBETAwMDQ1MzE1MDAwMDExNDk5AocBAYBXtXWZJWCYXTqDZWShpYqbCvt4ySidK8a6aTcbYuwbtDcaqXtbdp5H3Q9HAAZS9cn/qxQxGwBPihHaccUNJ/dcGQDXsKvpI0RW/w9AIS3FDbzJw7MSqQM1VZVEuF2CB87+tHTIHJ3l/bTc2U6ztG9USHBJW4PJDFtjjjUmpdGJMlq0LY8=";
+//		String string = "NTEBETUyMzE1NzU1ODQ4NDc0MDY2";
+		try {
+			byte[] code = decoder.decodeBuffer(string);
+			if(code[0] == '5' && code[1] == '1') {
+				byte[] newCode = new byte[code.length-2];
+				System.arraycopy(code, 2, newCode, 0, code.length-2);
+				code = newCode;
+				String s = new String(code, "UTF-8");
+				System.out.println(code);
+				System.out.println(s);
+				getSmartCardSegments(code);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static void getSmartCardSegments(byte[] code) {
+		int i = 0, len, typ;
+		Map<Integer, byte[]> segments = new HashMap<Integer, byte[]>();
+
+		while(i+2 < code.length) {
+			len = (int)code[i+1];
+			typ = (int)code[i];
+			if(len < 0 || (i+2+len) > code.length) {
+				break;
+			}
+			i += 2;
+			byte[] seg = new byte[len];
+			System.arraycopy(code, i, seg, 0, len);
+			segments.put(new Integer(typ), seg);
+			i += len;
+		}
 	}
 }
---
+++ src/main/java/keygenerator/otp/TestUrlEncoder.java
@@ -0,0 +1,21 @@
+package keygenerator.otp;
+
+import org.mortbay.util.UrlEncoded;
+
+/**
+ * @author moubin.mo
+ * @date: 2020/10/20 18:24
+ */
+
+public class TestUrlEncoder {
+	private final static UrlEncoded encoder = new UrlEncoded();
+
+	public static void main(String[] args) {
+		testUrlEnCode();
+	}
+	private final static void testUrlEnCode(){
+		String string = "51";
+		String encode = encoder.encode(string);
+		System.out.println(encode);
+	}
+}
