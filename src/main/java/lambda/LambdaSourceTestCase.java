a4adc494676f4cc68abf0479b04ce45b799456f3
scaummb <201330290918a>
2021-01-04 01:03:38 +0800

    字节码解析lambda使用。

---
+++ src/main/java/lambda/LambdaSourceTestCase.java
@@ -0,0 +1,14 @@
+package lambda;
+
+/**
+ * @author moubin.mo
+ * @date: 2021/1/3 23:11
+ */
+
+public class LambdaSourceTestCase {
+	public static void main(String[] args) {
+		String a = "a"; //定义局部变量
+		MyFunction myFunction = (s)-> {return s;}; //定义匿名函数实现
+		myFunction.print(a); //调用lambda
+	}
+}
