ef0ada8f9f300dc8877baea6f3525ac2a0bb0883
scaummb <201330290918a>
2021-02-01 22:25:51 +0800

    1. some testcase modify

---
+++ src/main/java/object/TestAddSelf.java
@@ -0,0 +1,14 @@
+package object;
+
+/**
+ * @author moubin.mo
+ * @date: 2021/2/1 20:34
+ */
+
+public class TestAddSelf {
+	public static void main(String[] args) {
+		int a = 1;
+		a = a++;
+		System.out.println(a);
+	}
+}
